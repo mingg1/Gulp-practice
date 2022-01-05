fb9e720bad05b6f56a785d91d89ce512b53815f8
Minji Choi <comg2843@gmail.com>
2022-01-06 00:51:00 +0200

    add scss task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,10 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import ws from "gulp-webserver";
 import image from "gulp-image";
+import autoPrefixer from "gulp-autoprefixer";
+import miniCSS from 'gulp-csso';
+
+const sass = require("gulp-sass")(require("node-sass"));
 
 const routes = {
   pug: {
@@ -14,11 +18,21 @@ const routes = {
     src: "src/img/*",
     dest: "build/img",
   },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
+  },
 };
 
-function pug() {
+const pug = () => {
   return gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
-}
+};
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on("error", sass.logError)).pipe(autoPrefixer()).pipe(miniCSS())
+    .pipe(gulp.dest(routes.scss.dest));
 
 const img = () =>
   gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
@@ -28,13 +42,15 @@ const clean = () => del(["build"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
+// tasks are when they are updated
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
-}; // tasks are when they are updated
+  gulp.watch(routes.scss.watch, styles);
+};
 
 const prepare = gulp.series([clean, img]);
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 const postDev = gulp.parallel([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
